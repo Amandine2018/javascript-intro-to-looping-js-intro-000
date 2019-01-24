7a45bc8970f75b87b4173485e2f8bcf709dcb91e
Amandine V <amavelamala@gmail.com>
2019-01-24 15:30:11 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,12 +1,9 @@
 var array = [];
 
 function forLoop(array){
-  for (let i = 1; i < 26; i++) {
-    if (i === 1){
-      array.push(`I am ${i} strange loop.`)
-    } else {
-      array.push(`I am ${i} strange loops.`)
-    }
+  for (let i = 0; i < 25, i++) {
+      array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`)`
+  } 
 }
 
 
